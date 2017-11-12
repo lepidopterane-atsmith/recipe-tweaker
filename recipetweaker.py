63664c9a502315efc1a00553138cdbfaa200a7d4
lepidopterane-atsmith <sabowitz@smith.edu>
2017-11-12 07:06:00 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	find-replace.py

--- recipetweaker.py
+++ recipetweaker.py
@@ -61,6 +61,19 @@ class Button:
 win = GraphWin("Recipe Tweaker",600,600)
 win.setCoords(-100,-100,100,100)
 
+customId = "<>"
+profile = dict([])
+
+gluten_ls = dict([["flour","black bean puree"],["breadcrumbs","ground flaxseed or crushed potato chips"],["flour tortillas","corn tortillas"]])
+sugar_ls = dict([["chocolate chips","cacao nibs"]])
+dairy_ls = dict([["butter","avocado puree or olive oil"],["milk","rice, soy, oat, or almond milk"],["heavy cream","coconut milk"],["cheese","soy cheese"],["cream cheese","dairy-free cream cheese"],["sour cream","dairy-free sour cream"]])
+vegan_ls = dict([["butter","avocado puree or olive oil"],["milk","almond milk"], ["heavy cream","coconut milk"],["ground beef","textured vegetable protein"]])
+lowso_ls = dict([["salt","herbs, citrus juice, or garlic powder"],["breadcrumbs","rolled oats"]])
+lgi_ls = dict([["rice","wild rice"],["penne","cheese tortellini"],["white bread","sourdough bread"]])
+ibs_ls = dict([["milk","rice, soy, or oat milk"],["cheese","soy cheese"],["cream cheese","dairy-free cream cheese"],["sour cream","dairy-free sour cream"],["ground beef","textured vegetable protein"],["mayonnaise","fat-free mayonnaise"],["salad dressing","fat-free salad dressing"]])
+
+pref_ls = []
+
 def start():
 	win.setBackground(color_rgb(255,235,225))
 	title = Text(Point(0,70),"Recipe Tweaker")
@@ -123,22 +136,41 @@ def choices(filename):
 	# ibs, irritable bowel syndrome, or both???
 	opt5 = Text(Point(0,-20),"Original Serving Number >> New Serving Number (ex. 4.5 >> 2)")
 	opt5.draw(win)
+	instr = "Or enter your custom profile: "+customId
+	opt9 = Text(Point(0,-25),instr)
+	opt9.draw(win)
 
 	but_ctnu = Button(win, Point(0,-60),40,10,'Continue')
 	but_ctnu.activate(True)
+	
+	but_cust = Button(win, Point(0,-75),40,10,"Custom")
+	but_cust.activate(True)
 
 	prefblank = Entry(Point(0,50),60)
 	prefblank.draw(win)
 	prefs = ''
 			
 	y = win.getMouse()
+	options = [opt1, opt2, opt3, opt4, opt5, opt6,opt7,opt8, opt9]
 
 	while(prefs=='' or (not but_ctnu.clicked(y))):
+		if (but_cust.clicked(y)):
+		    but_ctnu.deactivate()
+		    but_cust.deactivate()
+		    title.undraw()
+		    prefblank.undraw()
+		    dir.undraw()
+		    for i in options:
+		        i.setText("")
+		    customSetup(instr,filename)
+		    
 		y = win.getMouse()
 		print("boop")
 		prefs = prefblank.getText()+" \n"
 		
+		
 	but_ctnu.deactivate()
+	but_cust.deactivate()
 
 	raw = ["-","1","3/4","cups","confectioners\'","sugar","-","1","cup","almond","flour","-","3","large","egg","whites,","at","room","temperature","-","1/4","teaspoon","cream","of","tartar","-","Pinch","of","salt","-","1/4","cup","superfine","sugar","-","2","to","3","drops","gel","food","coloring","(see","below)","-","1/2","teaspoon","vanilla,","almond","or","mint","extract","-","Oven","with","convection","setting","-","4","baking","sheets","-","3","silicone","baking","mats","-","Fine-mesh","sieve","-","Pastry","bag","with","1/4-inch","round","tip","1.","Preheat","the","oven","to","300","degrees","F","using","the","convection","setting.","Line","3","baking","sheets","with","silicone","mats.","Measure","the","confectioners'","sugar","and","almond","flour","by","spooning","them","into","measuring","cups","and","leveling","with","a","knife.","Transfer","to","a","bowl;","whisk","to","combine.","2.","Sift","the","sugar-almond","flour","mixture,","a","little","at","a","time,","through","a","fine-mesh","sieve","into","a","large","bowl,","pressing","with","a","rubber","spatula","to","pass","through","as","much","as","possible.","It","will","take","a","while,","and","up","to","2","tablespoons","of","coarse","almond","flour","may","be","left;","just","toss","it.","3.","Beat","the","egg","whites,","cream","of","tartar","and","salt","with","a","mixer","on","medium","speed","until","frothy.","Increase","the","speed","to","medium","high;","gradually","add","the","superfine","sugar","and","beat","until","stiff","and","shiny,","about","5","more","minutes.","4.","Transfer","the","beaten","egg","whites","to","the","bowl","with","the","almond","flour","mixture.","Draw","a","rubber","spatula","halfway","through","the","mixture","and","fold","until","incorporated,","giving","the","bowl","a","quarter","turn","with","each","fold.","5.","Add","the","food","coloring","and","extract","(see","below).","Continue","folding","and","turning,","scraping","down","the","bowl,","until","the","batter","is","smooth","and","falls","off","the","spatula","in","a","thin","flat","ribbon,","2","to","3","minutes.","6.","Transfer","the","batter","to","a","pastry","bag","fitted","with","a","1/4-inch","round","tip.","Holding","the","bag","vertically","and","close","to","the","baking","sheet,","pipe","1","1/4-inch","circles","(24","per","sheet).","Firmly","tap","the","baking","sheets","twice","against","the","counter","to","release","any","air","bubbles.","7.","Let","the","cookies","sit","at","room","temperature","until","the","tops","are","no","longer","sticky","to","the","touch,","15","minutes","to","1","hour,","depending","on","the","humidity.","Slip","another","baking","sheet","under","the","first","batch","(a","double","baking","sheet","protects","the","cookies","from","the","heat).","8.","Bake","the","first","batch","until","the","cookies","are","shiny","and","rise","1/8","inch","to","form","a","foot","about","20","minutes.","Transfer","to","a","rack","to","cool","completely.","Repeat,","using","a","double","sheet","for","each","batch.","Peel","the","cookies","off","the","mats","and","sandwich","with","a","thin","layer","of","filling","(see","below).","9.","Tint","the","batter","with","2","drops","neon","pink","gel","food","coloring;","flavor","with","almond","extract.","Fill","with","seedless","raspberry","jam","(you'll","need","about","3/4","cup).","10.","Tint","the","batter","with","2","drops","mint","green","gel","food","coloring;","flavor","with","mint","extract.","For","the","filling,","microwave","3","ounces","chopped","white","chocolate,","2","tablespoons","heavy","cream","and","1","tablespoon","butter","in","30-second","intervals,","stirring,","until","smooth.","Stir","in","1/4","teaspoon","mint","extract","and","1","drop","mint","green","gel","food","coloring.","11.","Tint","the","batter","with","3","drops","royal","blue","gel","food","coloring;","flavor","with","vanilla","extract.","For","the","filling,","mix","4","ounces","softened","cream","cheese","and","3","tablespoons","blueberry","jam.","12.","Tint","the","batter","with","2","drops","violet","gel","food","coloring;","flavor","with","almond","or","vanilla","extract.","For","the","filling,","mix","3/4","cup","mascarpone","cheese,","2","tablespoons","Transfer","to","a","rack","to","cool","completely.","Repeat,","using","a","double","sheet","for","each","batch.","Peel","the","cookies","off","the","mats","and","sandwich","with","a","thin","layer","of","filling","(see","below).","9.","Tint","the","batter","with","2","drops","neon","pink","gel","food","coloring;","flavor","with","almond","extract.","Fill","with","seedless","raspberry","jam","(you'll","need","about","3/4","cup).","10.","Tint","the","batter","with","2","drops","mint","green","gel","food","coloring;","flavor","with","mint","extract.","For","the","filling,","microwave","3","ounces","chopped","white","chocolate,","2","tablespoons","heavy","cream","and","1","tablespoon","butter","in","30-second","intervals,","stirring,","until","smooth.","Stir","in","1/4","teaspoon","mint","extract","and","1","drop","mint","green","gel","food","coloring.","11.","Tint","the","batter","with","3","drops","royal","blue","gel","food","coloring;","flavor","with","vanilla","extract.","For","the","filling,","mix","4","ounces","softened","cream","cheese","and","3","tablespoons","blueberry","jam.","12.","Tint","the","batter","with","2","drops","violet","gel","food","coloring;","flavor","with","almond","or","vanilla","extract.","For","the","filling,","mix","3/4","cup","mascarpone","cheese,","2","tablespoons","Transfer","to","a","rack","to","cool","completely.","Repeat,","using","a","double","sheet","for","each","batch.","Peel","the","cookies","off","the","mats","and","sandwich","with","a","thin","layer","of","filling","(see","below).","9.","Tint","the","batter","with","2","drops","neon","pink","gel","food","coloring;","flavor","with","almond","extract.","Fill","with","seedless","raspberry","jam","(you'll","need","about","3/4","cup).","10.","Tint","the","batter","with","2","drops","mint","green","gel","food","coloring;","flavor","with","mint","extract.","For","the","filling,","microwave","3","ounces","chopped","white","chocolate,","2","tablespoons","heavy","cream","and","1","tablespoon","butter","in","30-second","intervals,","stirring,","until","smooth.","Stir","in","1/4","teaspoon","mint","extract","and","1","drop","mint","green","gel","food","coloring.","11.","Tint","the","batter","with","3","drops","royal","blue","gel","food","coloring;","flavor","with","vanilla","extract.","For","the","filling,","mix","4","ounces","softened","cream","cheese","and","3","tablespoons","blueberry","jam.","12.","Tint","the","batter","with","2","drops","violet","gel","food","coloring;","flavor","with","almond","or","vanilla","extract.","For","the","filling,","mix","3/4","cup","mascarpone","cheese,","2","tablespoons","honey","and","1","teaspoon","ground","dried","lavender."]
 	# I know this is long but also screw it I'm not gonna make this take up 25 lines of code    
@@ -146,12 +178,11 @@ def choices(filename):
 	title.undraw()
 	prefblank.undraw()
 	
-	options = [opt1, opt2, opt3, opt4, opt5, opt6,opt7,opt8]
-
 	for i in options:
 		i.undraw()
 
 	dir.undraw()
+	prefilter(prefs)
 	pages(filename, prefs, raw)
 
 def pages(filename, prefs, raw):
@@ -171,8 +202,22 @@ def pages(filename, prefs, raw):
 	but_quit = Button(win, Point(-80,-80),20,20,'Quit')
 	but_again = Button(win, Point(80,-80),20,20,'Restart')
 	but_retweak = Button(win, Point(40,-80),30,20,'Retweak')
-	# ask Stef about this one
-
+	
+	word_ls = []
+	
+	file_token = open(filename,"r")
+	for file in file_token:
+		body = file.split("-") # why split here?
+        for line in body:
+            sentc = line.split(",") # why split here??
+	new_file = " ".join(body)
+    
+	for key in dictionary_lst.iterkeys(): # warning: DNE
+            recur_replace(key, new_file, pref_ls)
+	print("outer ", new_file)
+	raw = new_file.split(" ")
+    
+	# raw recipe needs to be defined here before we can continue
 	but_arr = [but_next,but_quit,but_again, but_retweak]
 	for i in but_arr:
 		i.activate(True)
@@ -328,5 +373,102 @@ def render(raw):
 		print(i+"\n");
 	'''
 	return processed
+
+def customSetup(instr,filename):
+    title = Text(Point(0,85),"Customize Your Tweak")
+    title.setSize(18)
+    title.setStyle("bold italic")
+    title.draw(win)
+    
+    instr = instr.replace("Or e","E").replace(":"," name:")
+    iDMessage = Text(Point(0,70),instr)
+    iDMessage.draw(win)
+    
+    iDEntry = Entry(Point(0,60),30)
+    iDEntry.draw(win)
+    
+    modTitle = Text(Point(0,35),"Mods")
+    modTitle.setStyle("bold")
+    modTitle.draw(win)
+    
+    exp = Text(Point(0,25),"List like so: ingredient 1, replacement 1, ingredient 2, replacement 2...")
+    exp.draw(win)
+    
+    expEntry = Entry(Point(0,10),60)
+    expEntry.draw(win)
+    
+    back = Button(win, Point(0,-60),40,10,"Back")
+    back.activate(True)
+    
+    y = win.getMouse()
+    
+    while (not back.clicked(y)):
+        y = win.getMouse()
+    
+    if (not iDEntry.getText() == ""):
+        customId = iDEntry.getText()
+    
+    protostr = expEntry.getText()
+    
+    if (not protostr == ""):
+        protofile = protostr.split(",")
+        plen = len(protofile)
+        container = []
+    
+        for i in range(1,plen,2):
+            templist = [protofile[i-1],protofile[i]]
+            container.append(templist)
+    
+        profile  = dict(container)
+        
+    back.deactivate()    
+    erase = [title, iDMessage, iDEntry, modTitle, exp, expEntry]
+    for i in erase:
+        i.undraw()
+    
+    choices(filename)
+
+def recur_replace(key, file, dictionary_lst):
+    if key == len(dictionary_lst):
+        return "Done"
+    else:
+        value_ls=[]
+        for val in dictionary_lst.itervalues():
+            value_ls.append(val)
+        for i in value_ls[:]:
+            print("inner ",file.replace(key, i))
+            value_ls.remove(i)
+            print(value_ls)
+        dictionary_lst.pop(key) 
+        # Key Error - you already popped the butter when you want to pop it again
+        return recur_replace(key, file, dictionary_lst)
+
+def prefilter(prefs):
+    if (prefs == "Gluten-Free"):
+        pref_ls = gluten_ls
+    if (prefs == "Sugar-Free"):
+        pref_ls = sugar_ls
+    if (prefs == "Dairy-Free"):
+        pref_ls = dairy_ls
+    if (prefs == "Vegan"):
+        pref_ls = vegan_ls
+    if (prefs == "Low-Sodium"):
+        pref_ls = lowso_ls
+    if (prefs == "Low Glycemic Index"):
+        pref_ls = lgi_ls
+    if (prefs == "Irritable Bowel Syndrome"):
+        pref_ls = ibs_ls
+    if (prefs == customId):
+        pref_ls = profile
+
+def divver(division):
+    division.replace("/"," ")
+    divarr = division.split()
+    divlen = len(divarr)
+    
+    x = double(divarr[0])
+    y = double(divarr[1])
+    
+    return (x/y)
 
 start()
